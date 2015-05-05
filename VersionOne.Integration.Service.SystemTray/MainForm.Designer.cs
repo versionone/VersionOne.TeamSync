8f18a63b6fc09db0a28c450783bf612801848e84
Acey Bunch <acey.bunch@versionone.com>
2015-05-05 06:51:23 -0400

    Replaced graphic files

--- VersionOne.Integration.Service.SystemTray/MainForm.Designer.cs
+++ VersionOne.Integration.Service.SystemTray/MainForm.Designer.cs
@@ -32,10 +32,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SystemTray));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.startServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.stopServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.v1logoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewActivityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configureServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.startServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recycleServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitServiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,50 +49,94 @@
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "VersionOne Integration Service";
             this.notifyIcon1.Visible = true;
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.v1logoToolStripMenuItem,
+            this.viewActivityToolStripMenuItem,
+            this.configureServiceToolStripMenuItem,
+            this.toolStripSeparator1,
             this.startServiceToolStripMenuItem,
+            this.pauseServiceToolStripMenuItem,
+            this.recycleServiceToolStripMenuItem,
             this.stopServiceToolStripMenuItem,
+            this.toolStripSeparator2,
             this.exitServiceToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(237, 114);
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(230, 214);
+            // 
+            // v1logoToolStripMenuItem
+            // 
+            this.v1logoToolStripMenuItem.AutoSize = false;
+            this.v1logoToolStripMenuItem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("v1logoToolStripMenuItem.BackgroundImage")));
+            this.v1logoToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.v1logoToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.v1logoToolStripMenuItem.Enabled = false;
+            this.v1logoToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.v1logoToolStripMenuItem.Name = "v1logoToolStripMenuItem";
+            this.v1logoToolStripMenuItem.ShowShortcutKeys = false;
+            this.v1logoToolStripMenuItem.Size = new System.Drawing.Size(236, 22);
+            this.v1logoToolStripMenuItem.Text = "VersionOne Integration Service";
+            // 
+            // viewActivityToolStripMenuItem
+            // 
+            this.viewActivityToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.viewActivityToolStripMenuItem.Name = "viewActivityToolStripMenuItem";
+            this.viewActivityToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.viewActivityToolStripMenuItem.Text = "View Activity...";
+            // 
+            // configureServiceToolStripMenuItem
+            // 
+            this.configureServiceToolStripMenuItem.Name = "configureServiceToolStripMenuItem";
+            this.configureServiceToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.configureServiceToolStripMenuItem.Text = "Configure...";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(226, 6);
             // 
             // startServiceToolStripMenuItem
             // 
             this.startServiceToolStripMenuItem.Name = "startServiceToolStripMenuItem";
-            this.startServiceToolStripMenuItem.Size = new System.Drawing.Size(236, 22);
-            this.startServiceToolStripMenuItem.Text = "Start service";
+            this.startServiceToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.startServiceToolStripMenuItem.Text = "Start";
             this.startServiceToolStripMenuItem.Click += new System.EventHandler(this.startServiceToolStripMenuItem_Click);
+            // 
+            // pauseServiceToolStripMenuItem
+            // 
+            this.pauseServiceToolStripMenuItem.Name = "pauseServiceToolStripMenuItem";
+            this.pauseServiceToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.pauseServiceToolStripMenuItem.Text = "Pause";
+            // 
+            // recycleServiceToolStripMenuItem
+            // 
+            this.recycleServiceToolStripMenuItem.Name = "recycleServiceToolStripMenuItem";
+            this.recycleServiceToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.recycleServiceToolStripMenuItem.Text = "Recycle";
             // 
             // stopServiceToolStripMenuItem
             // 
             this.stopServiceToolStripMenuItem.Name = "stopServiceToolStripMenuItem";
-            this.stopServiceToolStripMenuItem.Size = new System.Drawing.Size(236, 22);
-            this.stopServiceToolStripMenuItem.Text = "Stop service";
+            this.stopServiceToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.stopServiceToolStripMenuItem.Text = "Stop";
             this.stopServiceToolStripMenuItem.Click += new System.EventHandler(this.stopServiceToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(226, 6);
             // 
             // exitServiceToolStripMenuItem
             // 
             this.exitServiceToolStripMenuItem.Name = "exitServiceToolStripMenuItem";
-            this.exitServiceToolStripMenuItem.Size = new System.Drawing.Size(236, 22);
+            this.exitServiceToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
             this.exitServiceToolStripMenuItem.Text = "Exit";
             this.exitServiceToolStripMenuItem.Click += new System.EventHandler(this.exitServiceToolStripMenuItem_Click);
-            // 
-            // v1logoToolStripMenuItem
-            // 
-            this.v1logoToolStripMenuItem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("v1logoToolStripMenuItem.BackgroundImage")));
-            this.v1logoToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.v1logoToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.v1logoToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.v1logoToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.v1logoToolStripMenuItem.Name = "v1logoToolStripMenuItem";
-            this.v1logoToolStripMenuItem.Size = new System.Drawing.Size(236, 22);
-            this.v1logoToolStripMenuItem.Text = "VersionOne Integration Service";
             // 
             // SystemTray
             // 
@@ -112,6 +162,12 @@
         private System.Windows.Forms.ToolStripMenuItem stopServiceToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitServiceToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem v1logoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configureServiceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewActivityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem pauseServiceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recycleServiceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
     }
 }
 
