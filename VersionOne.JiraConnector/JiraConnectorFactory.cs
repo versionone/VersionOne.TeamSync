148f6806c4efe864c4109ccde5cf93da48d56857
Acey Bunch <acey.bunch@versionone.com>
2015-05-11 13:33:34 -0400

    Added JIRAConnector

---
+++ VersionOne.JiraConnector/JiraConnectorFactory.cs
@@ -0,0 +1,32 @@
+﻿/*(c) Copyright 2012, VersionOne, Inc. All rights reserved. (c)*/
+using System;
+using VersionOne.JiraConnector.Rest;
+using VersionOne.JiraConnector.Soap;
+
+namespace VersionOne.JiraConnector
+{
+    public class JiraConnectorFactory
+    {
+        public readonly JiraConnectorType ConnectorType;
+
+        public JiraConnectorFactory(JiraConnectorType connectorType)
+        {
+            ConnectorType = connectorType;
+        }
+
+        public IJiraConnector Create(string url, string username, string password)
+        {
+            switch (ConnectorType)
+            {
+                case JiraConnectorType.Soap:
+                    return new JiraSoapProxy(url, username, password);
+
+                case JiraConnectorType.Rest:
+                    return new JiraRestProxy(url, username, password);
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
