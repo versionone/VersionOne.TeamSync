d0324a4e2d84a0a1c2093fc9713a45a318aade31
Santiago Aceñolaza <chanacenolaza@gmail.com>
2015-05-22 11:01:22 -0300

    S-52888 Added configuration settings to app.config file

---
+++ VersionOne.Integration.Service.Core/Config/ServiceSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionOne.Integration.Service.Core.Config
+{
+    public class ServiceSettings : ConfigurationSection
+    {
+        public static readonly ServiceSettings Settings = ConfigurationManager.GetSection("serviceSettings") as ServiceSettings;
+
+        [ConfigurationProperty("syncInterval", DefaultValue = 5000, IsRequired = true)]
+        public int SyncInterval
+        {
+            get { return (int)this["syncInterval"]; }
+            set { this["syncInterval"] = value; }
+        }
+    }
+}
---
+++ VersionOne.SDK.Jira/Config/JiraSettings.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionOne.SDK.Jira.Config
+{
+    public class JiraSettings : ConfigurationSection
+    {
+        public static readonly JiraSettings Settings = ConfigurationManager.GetSection("jiraSettings") as JiraSettings;
+
+        [ConfigurationProperty("servers", IsDefaultCollection = true)]
+        public JiraServerCollection Servers
+        {
+            get
+            {
+                return (JiraServerCollection)this["servers"];
+            }
+            set
+            {
+                this["servers"] = value;
+            }
+        }
+    }
+
+    public class JiraServer : ConfigurationElement
+    {
+        [ConfigurationProperty("enabled", IsRequired = true, DefaultValue = true)]
+        public bool Enabled
+        {
+            get { return (bool)this["enabled"]; }
+            set { this["enabled"] = value; }
+        }
+
+        [ConfigurationProperty("name", DefaultValue = "", IsKey = true, IsRequired = true)]
+        public string name
+        {
+            get { return (string)base["name"]; }
+            set { base["name"] = value; }
+        }
+
+        [ConfigurationProperty("url", IsRequired = true)]
+        public string Url
+        {
+            get { return (string)this["url"]; }
+            set { this["url"] = value; }
+        }
+
+        [ConfigurationProperty("username", IsRequired = true)]
+        public string Username
+        {
+            get { return (string)this["username"]; }
+            set { this["username"] = value; }
+        }
+
+        [ConfigurationProperty("password", IsRequired = true)]
+        public string Password
+        {
+            get { return (string)this["password"]; }
+            set { this["password"] = value; }
+        }
+
+        [ConfigurationProperty("projectMappings")]
+        public ProjectMappingCollection ProjectMappings
+        {
+            get
+            {
+                return (ProjectMappingCollection)this["projectMappings"];
+            }
+            set
+            {
+                this["projectMappings"] = value;
+            }
+        }
+    }
+
+    [ConfigurationCollection(typeof(JiraServer), CollectionType = ConfigurationElementCollectionType.BasicMapAlternate)]
+    public class JiraServerCollection : ConfigurationElementCollection
+    {
+        internal const string PropertyName = "server";
+
+        protected override string ElementName
+        {
+            get
+            {
+                return PropertyName;
+            }
+        }
+
+        protected override bool IsElementName(string elementName)
+        {
+            return elementName.Equals(PropertyName, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new JiraServer();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((JiraServer)(element)).name;
+        }
+
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get
+            {
+                return ConfigurationElementCollectionType.BasicMapAlternate;
+            }
+        }
+
+        public override bool IsReadOnly()
+        {
+            return false;
+        }
+
+        public JiraServer this[int idx]
+        {
+            get
+            {
+                return (JiraServer)BaseGet(idx);
+            }
+        }
+    }
+
+    public class ProjectMapping : ConfigurationElement
+    {
+        [ConfigurationProperty("enabled", IsRequired = true, DefaultValue = true)]
+        public bool Enabled
+        {
+            get { return (bool)this["enabled"]; }
+            set { this["enabled"] = value; }
+        }
+
+        [ConfigurationProperty("v1Project", IsRequired = true)]
+        public string V1Project
+        {
+            get { return (string)this["v1Project"]; }
+            set { this["v1Project"] = value; }
+        }
+
+        [ConfigurationProperty("jiraProject", IsRequired = true)]
+        public string JiraProject
+        {
+            get { return (string)this["jiraProject"]; }
+            set { this["jiraProject"] = value; }
+        }
+    }
+
+    [ConfigurationCollection(typeof(ProjectMapping), CollectionType = ConfigurationElementCollectionType.BasicMapAlternate)]
+    public class ProjectMappingCollection : ConfigurationElementCollection
+    {
+        internal const string PropertyName = "project";
+
+        protected override string ElementName
+        {
+            get
+            {
+                return PropertyName;
+            }
+        }
+
+        protected override bool IsElementName(string elementName)
+        {
+            return elementName.Equals(PropertyName, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new ProjectMapping();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((ProjectMapping)(element)).V1Project + "/" + ((ProjectMapping)(element)).JiraProject;
+        }
+
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get
+            {
+                return ConfigurationElementCollectionType.BasicMapAlternate;
+            }
+        }
+
+        public override bool IsReadOnly()
+        {
+            return false;
+        }
+
+        public ProjectMapping this[int idx]
+        {
+            get
+            {
+                return (ProjectMapping)BaseGet(idx);
+            }
+        }
+    }
+}
