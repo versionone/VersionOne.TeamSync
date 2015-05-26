f586a16fd1d2a9c6129f716dbab7fa56d4587a72
Jesse <jeriley@gmail.com>
2015-05-26 15:09:56 -0400

    remove unused statements

--- VersionOne.Integration.Service/Service1.cs
+++ VersionOne.Integration.Service/Service1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
 using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
 using VersionOne.Integration.Service.Core;
 using VersionOne.Integration.Service.Core.Config;
