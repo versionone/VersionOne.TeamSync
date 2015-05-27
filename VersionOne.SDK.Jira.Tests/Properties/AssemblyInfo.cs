a15a077e21aba09ae4aea5877d6513b231569a1f
Acey Bunch <acey.bunch@versionone.com>
2015-05-27 15:59:05 -0400

    Renamed JiraConnector

--- VersionOne.SDK.Jira.Tests/Properties/AssemblyInfo.cs
+++ VersionOne.SDK.Jira.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VersionOne.SDK.Jira.Tests")]
+[assembly: AssemblyTitle("VersionOne.JiraConnector.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VersionOne.SDK.Jira.Tests")]
+[assembly: AssemblyCompany("VersionOne")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
