c026a44f6083cfd0a2f3f906f6e239df9effd185
Jesse <jeriley@gmail.com>
2015-06-09 16:13:11 -0400

    Extract out meta data from the instance

---
+++ VersionOne.TeamSync.Core.Tests/JsonConverter/MetaDataConverterTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Should;
+using VersionOne.TeamSync.JiraConnector.Connector;
+using VersionOne.TeamSync.JiraConnector.Entities;
+
+namespace VersionOne.TeamSync.Core.Tests.JsonConverter
+{
+    [TestClass]
+    public class MetaDataConverterTests
+    {
+        private CreateMeta _result;
+
+        [TestMethod]
+        public void should_have_one_project()
+        {
+            _result.Projects.Count.ShouldEqual(1);
+        }
+
+        [TestMethod]
+        public void should_have_5_sets_of_issues()
+        {
+            _result.Projects.Single().IssueTypes.Count.ShouldEqual(5);
+        }
+
+        [TestMethod]
+        public void epic_type_should_have_15_properties()
+        {
+            _result.Projects.Single()
+                .Epic
+                .Fields
+                .Properties.Count.ShouldEqual(15);
+        }
+
+        [TestMethod]
+        public void epic_type_should_have_3_official_custom_fields()
+        {
+            _result.Projects.Single()
+                .OfficialEpicCustomFields
+                .Count.ShouldEqual(3);
+        }
+
+        [TestMethod]
+        public void epic_name_should_have_the_property_key()
+        {
+            _result.Projects.Single().EpicName.Key.ShouldEqual("customfield_10006");
+        }
+
+        [TestInitialize]
+        public void context()
+        {
+            var jsonResult = File.ReadAllText("JsonConverter/CreateMeta.json");
+            var jsonSerializerSettings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = new LowercaseContractResolver(),
+            };
+            //jsonSerializerSettings.Converters.Add(new MetaData());
+            _result = JsonConvert.DeserializeObject<CreateMeta>(jsonResult, jsonSerializerSettings);
+
+        }
+
+    }
+}
---
+++ VersionOne.TeamSync.JiraConnector/Entities/CreateMeta.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace VersionOne.TeamSync.JiraConnector.Entities
+{
+    public class CreateMeta
+    {
+        public CreateMeta()
+        {
+            Projects = new List<MetaProject>();
+        }
+
+        public string Name { get; set; }
+        public List<MetaProject> Projects { get; set; }
+
+    }
+
+    public class MetaProject
+    {
+        public MetaProject()
+        {
+            IssueTypes = new List<MetaIssueType>();   
+        }
+        public string Key { get; set; }
+        public List<MetaIssueType> IssueTypes { get; set; }
+
+        public MetaIssueType Epic
+        {
+            get
+            {
+                return IssueTypes.SingleOrDefault(x => x.Name == "Epic");
+            }
+        }
+
+        public List<MetaProperty> OfficialEpicCustomFields
+        {
+            get
+            {
+                return Epic.Fields.Properties.Where(x => !string.IsNullOrWhiteSpace(x.Schema) && x.Schema.StartsWith("com.pyxis.greenhopper.jira:gh")).ToList();
+            }
+        }
+
+        public MetaProperty EpicName
+        {
+            get { return OfficialEpicCustomFields.FirstOrDefault(x => x.Property == "Epic Name"); }
+        }
+    }
+
+    public class MetaIssueType
+    {
+        public string Name { get; set; }
+        public MetaField Fields { get; set; } 
+    }
+
+
+    [JsonConverter(typeof(MetaData))]
+    public class MetaField
+    {
+        public MetaField()
+        {
+            Properties = new List<MetaProperty>();
+        }
+        public List<MetaProperty> Properties { get; set; } 
+    }
+
+    public class MetaProperty
+    {
+        public string Property { get; set; }
+        public string Schema { get; set; }
+        public string Key { get; set; }
+    }
+
+    public class MetaData : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var item = JObject.Load(reader).ToObject<Dictionary<string, object>>();
+            //var properties = item.Properties().ToList();
+            var resultList = new List<MetaProperty>();
+
+            foreach (var key in item.Keys)
+            {
+                dynamic meta = item[key];
+                var name = meta.name;
+                var customNamespace = meta.schema.custom;
+
+                resultList.Add(new MetaProperty()
+                {
+                    Key = key,
+                    Property = name,
+                    Schema = customNamespace
+                });
+            }
+            
+            return new MetaField()
+            {
+                Properties = resultList
+            };
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
