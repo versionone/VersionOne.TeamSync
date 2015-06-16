eef95a5b326fe4cbfff184087841d771a2725c24
Jesse <jeriley@gmail.com>
2015-06-16 08:31:42 -0400

    updated test
    ...this'll never happen ever again

--- VersionOne.TeamSync.Core.Tests/V1DomainTests.cs
+++ VersionOne.TeamSync.Core.Tests/V1DomainTests.cs
@@ -97,7 +97,7 @@ namespace VersionOne.TeamSync.Core.Tests
         {
             var mockConnector = new Mock<IV1Connector>();
             mockConnector.Setup(x => x.Query("Story",
-                new[] { "ID.Number", "Reference", "AssetState" },
+                new[] { "ID.Number", "Reference", "IsInactive", "AssetState" },
                 new[] {"Reference!=\"\"", "Scope=\"Scope:1000\""},
                 Story.FromQuery))
                 .ReturnsAsync(new List<Story>());
