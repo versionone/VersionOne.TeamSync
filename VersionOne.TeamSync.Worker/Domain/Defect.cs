d174f9e8bf1b3df0ac4a8e34dc974b7767cb959c
Jesse <jeriley@gmail.com>
2015-07-09 14:32:31 -0400

    dead code

--- VersionOne.TeamSync.Worker/Domain/Defect.cs
+++ VersionOne.TeamSync.Worker/Domain/Defect.cs
@@ -14,35 +14,7 @@ namespace VersionOne.TeamSync.Worker.Domain
 			get { return "Defect"; }
 		}
 
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Defect)obj);
-        }
-
-        protected bool Equals(Defect other)
-        {
-            return string.Equals(Name, other.Name) && string.Equals(Description, other.Description) && string.Equals(Estimate, other.Estimate) && string.Equals(ToDo, other.ToDo) && string.Equals(Reference, other.Reference) && string.Equals(Super, other.Super);
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = (Name != null ? Name.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (Description != null ? Description.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (Estimate != null ? Estimate.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (ToDo != null ? ToDo.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (Reference != null ? Reference.GetHashCode() : 0);
-                hashCode = (hashCode * 397) ^ (Super != null ? Super.GetHashCode() : 0);
-                return hashCode;
-            }
-        }
-
-
-		public string ID { get; set; }
+        public string ID { get; set; }
 		public string Error { get; private set; }
 		public bool HasErrors { get; private set; }
         public string Name { get; set; }
