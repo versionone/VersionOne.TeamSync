64488a774d0dd62f212a7db625b6f8a2f966f072
Pablo Freiberg <pablo.freiberg@gmail.com>
2015-07-17 16:23:25 -0300

    S-54295: teamSync config file scrub revisit

--- pre.001.ScrubConfigFile.ps1
+++ pre.001.ScrubConfigFile.ps1
@@ -11,36 +11,36 @@ function Clean-ConfigFile {
             $_.filter.level.value = "ALL"
         } else{
             if ($_.name -eq "RollingLogFileAppender") {
-                $_.filter.level.value = "INFO"
+                $_.filter.level.value = "WARN"
             }
         } 
      }
 
     ## serviceSettings
-    $xml.configuration.serviceSettings.syncIntervalInSeconds = "600"
+    $xml.configuration.serviceSettings.syncIntervalInSeconds = "1800"
 
 	## v1Settings
     $xml.configuration.v1Settings.authenticationType = "0"
 	$xml.configuration.v1Settings.url = "http://server/instance"
-	$xml.configuration.v1Settings.accessToken = "accessToken"
-	$xml.configuration.v1Settings.username = "username"
-	$xml.configuration.v1Settings.password = "password"
+	$xml.configuration.v1Settings.accessToken = ""
+	$xml.configuration.v1Settings.username = ""
+	$xml.configuration.v1Settings.password = ""
 
     ## jiraSettings
     $xml.configuration.jiraSettings.servers.server | % {
         if ($_ -eq $xml.configuration.jiraSettings.servers.server[0]) {
-            $_.enabled = ""
+            $_.enabled = "false"
             $_.name = ""
             $_.url = "http://server/instance"
-            $_.username = "username"
-            $_.password = "password"
+            $_.username = ""
+            $_.password = ""
             $firstMapping = $_.projectMappings.project[0]
             $_.projectMappings.project | % {
                 if ($_ -eq $firstMapping) {
-                    $_.enabled = ""
+                    $_.enabled = "false"
                     $_.v1Project = ""
                     $_.jiraProject = ""
-                    $_.epicSyncType = ""
+                    $_.epicSyncType = "EpicCategory:208"
                 } else {
                     $firstMapping.ParentNode.RemoveChild($_)
                 }
