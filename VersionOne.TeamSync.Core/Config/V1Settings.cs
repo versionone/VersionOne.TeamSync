8668566a868c0da7ddbc3b021669012d8d77d4a0
Santiago Aceñolaza <chanacenolaza@gmail.com>
2015-07-22 13:42:49 -0300

    S-54339: TeamSync supports using a proxy

--- VersionOne.TeamSync.Core/Config/V1Settings.cs
+++ VersionOne.TeamSync.Core/Config/V1Settings.cs
@@ -58,42 +58,4 @@ namespace VersionOne.TeamSync.Core.Config
             set { this["proxy"] = value; }
         }
     }
-
-    public class Proxy : ConfigurationElement
-    {
-        [ConfigurationProperty("enabled", IsRequired = true, DefaultValue = false)]
-        public bool Enabled
-        {
-            get { return (bool)this["enabled"]; }
-            set { this["enabled"] = value; }
-        }
-
-        [ConfigurationProperty("url", IsRequired = true)]
-        public string Url
-        {
-            get { return (string)this["url"]; }
-            set { this["url"] = value; }
-        }
-
-        [ConfigurationProperty("username", IsRequired = true)]
-        public string Username
-        {
-            get { return (string)this["username"]; }
-            set { this["username"] = value; }
-        }
-
-        [ConfigurationProperty("password", IsRequired = true)]
-        public string Password
-        {
-            get { return (string)this["password"]; }
-            set { this["password"] = value; }
-        }
-
-        [ConfigurationProperty("domain", IsRequired = false)]
-        public string Domain
-        {
-            get { return (string)this["domain"]; }
-            set { this["domain"] = value; }
-        }
-    }
 }
