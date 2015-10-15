1b9bdba4f9438a72924141273c4860557c0aabc4
Jesse <jeriley@gmail.com>
2015-10-15 15:43:07 -0400

    tests for 1000 or less results from a query

--- VersionOne.TeamSync.JiraConnector.Tests/SearchResultPagingTests.cs
+++ VersionOne.TeamSync.JiraConnector.Tests/SearchResultPagingTests.cs
@@ -15,7 +15,7 @@ using VersionOne.TeamSync.JiraConnector.Entities;
 namespace VersionOne.TeamSync.JiraConnector.Tests
 {
     [TestClass]
-    public class SearchResultPagingTests
+    public class SearchResult_more_than_1000_results_Tests
     {
         private List<IRestRequest> _restRequest = new List<IRestRequest>();
         private Mock<IRestResponse>[] _restResponse;
@@ -69,7 +69,8 @@ namespace VersionOne.TeamSync.JiraConnector.Tests
 
             _result = connector.GetAllSearchResults(new Dictionary<string, IEnumerable<string>>
             {
-                {"project",new[]{"WAT"}}
+                {"project",new[]{"WAT", "HUH"}},
+                {"stuff",new[]{"whos-there"}}
             }, 
                 new[] { "lulz" }
             );
@@ -106,7 +107,7 @@ namespace VersionOne.TeamSync.JiraConnector.Tests
         [TestMethod]
         public void first_request_should_have_jql_with_a_project_of_WAT()
         {
-            _firstRequest.Parameters.Single(x => x.Name == "jql").Value.ShouldEqual("project=WAT");
+            _firstRequest.Parameters.Single(x => x.Name == "jql").Value.ShouldEqual("project in (WAT, HUH) AND stuff=whos-there");
         }
 
         [TestMethod]
@@ -124,7 +125,7 @@ namespace VersionOne.TeamSync.JiraConnector.Tests
         [TestMethod]
         public void second_request_should_have_jql_with_a_project_of_WAT()
         {
-            _secondRequest.Parameters.Single(x => x.Name == "jql").Value.ShouldEqual("project=WAT");
+            _secondRequest.Parameters.Single(x => x.Name == "jql").Value.ShouldEqual("project in (WAT, HUH) AND stuff=whos-there");
         }
 
         [TestMethod]
@@ -145,4 +146,102 @@ namespace VersionOne.TeamSync.JiraConnector.Tests
             _secondRequest.Parameters.Single(x => x.Name == "startAt").Value.ShouldEqual("1001");
         }
     }
+
+
+    [TestClass]
+    public class SearchResult_1000_or_less_results_Tests
+    {
+        private Mock<IRestResponse> _restResponse;
+        private Mock<ILog> _mockLog;
+        private Mock<IRestClient> _restClient;
+
+        private string _overloadedResponse = "{\"startAt\":0,\"maxResults\":1000,\"total\":1000,\"issues\":[{\"id\":5 }]}";
+
+        private SearchResult _result;
+        private IRestRequest _request;
+
+        private Connector.JiraConnector CreateConnect()
+        {
+            _restClient = new Mock<IRestClient>();
+            _restResponse = MakeAResponse(HttpStatusCode.OK, _overloadedResponse);
+            
+            _restClient.Setup(x => x.BaseUrl).Returns(new Uri("http://baseUrl"));
+            _restClient.Setup(x => x.Execute(It.IsAny<IRestRequest>())).Returns((IRestRequest request) =>
+            {
+                _request = request;
+                return _restResponse.Object;
+            });
+
+            _mockLog = new Mock<ILog>();
+            _mockLog.SetupGet(x => x.Logger).Returns(new Mock<ILogger>().Object);
+
+            return new JiraConnector.Connector.JiraConnector(_restClient.Object, _mockLog.Object);
+        }
+
+        private Mock<IRestResponse> MakeAResponse(HttpStatusCode expectedCode, string content)
+        {
+            var response = new Mock<IRestResponse>();
+            response.Setup(x => x.StatusCode).Returns(expectedCode);
+            response.Setup(x => x.Content).Returns(content);
+            response.Setup(x => x.Headers).Returns(new List<Parameter>());
+            return response;
+        }
+
+        [TestInitialize]
+        public void Context()
+        {
+            var connector = CreateConnect();
+
+            _result = connector.GetAllSearchResults(new Dictionary<string, IEnumerable<string>>
+            {
+                {"project",new[]{"WAT", "HUH"}},
+                {"stuff",new[]{"whos-there"}}
+            },
+                new[] { "lulz" }
+            );
+        }
+
+        [TestMethod]
+        public void should_have_called_get_twice()
+        {
+            _restClient.Verify(x => x.Execute(It.IsAny<IRestRequest>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void should_have_1_issues()
+        {
+            _result.issues.Count.ShouldEqual(1);
+        }
+
+        [TestMethod]
+        public void should_have_the_one_id_for_issue()
+        {
+            _result.issues.Single().id.ShouldEqual("5");
+        }
+
+        [TestMethod]
+        public void should_not_log_a_message_that_theres_a_buncha_stuff()
+        {
+            _mockLog.Verify(x => x.Info(It.IsAny<string>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void request_should_have_jql_with_a_project_of_WAT()
+        {
+            _request.Parameters.Single(x => x.Name == "jql").Value.ShouldEqual("project in (WAT, HUH) AND stuff=whos-there");
+        }
+
+        [TestMethod]
+        public void request_should_have_maxResults_number_of_1000()
+        {
+            _request.Parameters.Single(x => x.Name == "maxResults").Value.ShouldEqual("1000");
+        }
+
+        [TestMethod]
+        public void request_should_have_fields_of_lulz()
+        {
+            _request.Parameters.Single(x => x.Name == "fields").Value.ShouldEqual("lulz");
+        }
+    }
+
 }
