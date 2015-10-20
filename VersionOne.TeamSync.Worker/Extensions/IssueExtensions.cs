d747f8d1a23df37226241dc71db2c97f74c10e23
Jesse <jeriley@gmail.com>
2015-10-20 15:28:14 -0400

    leftovers from merge

--- VersionOne.TeamSync.Worker/Extensions/IssueExtensions.cs
+++ VersionOne.TeamSync.Worker/Extensions/IssueExtensions.cs
@@ -74,12 +74,11 @@ namespace VersionOne.TeamSync.Worker.Extensions
         public static bool ItMatchesDefect(this Issue issue, Defect defect)
         {
             return string.Equals(defect.Name, issue.Fields.Summary.Trim()) &&
-                string.Equals(defect.Description, issue.RenderedFields.Description.ToEmptyIfNull()) &&
-                string.Equals(defect.Estimate, issue.Fields.StoryPoints.ToEmptyIfNull()) &&
-                string.Equals(defect.ToDo, issue.Fields.RemainingInDays.ToEmptyIfNull()) &&
-                string.Equals(defect.Reference, issue.Key) &&
-                string.Equals(defect.SuperNumber, issue.Fields.EpicLink.ToEmptyIfNull()) &&
-                string.Equals(defect.Priority, issue.Fields.Priority.ToEmptyIfNull());
+                   string.Equals(defect.Description, issue.RenderedFields.Description.ToEmptyIfNull()) &&
+                   string.Equals(defect.Estimate, issue.Fields.StoryPoints.ToEmptyIfNull()) &&
+                   string.Equals(defect.ToDo, issue.Fields.RemainingInDays.ToEmptyIfNull()) &&
+                   string.Equals(defect.Reference, issue.Key) &&
+                   string.Equals(defect.SuperNumber, issue.Fields.EpicLink.ToEmptyIfNull());
         }
 
         public static bool HasAssignee(this Issue issue)
