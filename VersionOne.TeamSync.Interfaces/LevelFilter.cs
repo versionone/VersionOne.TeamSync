90db97c497040de686eaf5b2ced8d04b827d4c32
Josh Gough <jsgough@gmail.com>
2015-12-17 14:31:24 -0500

    S-52892 Upgraded to log4net 1.2.15.0; WIP to decouple from log4net hard dependency
    First removing references to TeamSync.Core by moving some small configuration classes into Interfaces, and other classes into the only projects within which they are used.

---
+++ VersionOne.TeamSync.Interfaces/LevelFilter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using log4net.Core;
+using log4net.Filter;
+
+namespace VersionOne.TeamSync.Core
+{
+	public class LevelFilter : FilterSkeleton
+	{
+		private Level _level;
+
+		public Level Level
+		{
+			get { return _level; }
+			set { _level = value; }
+		}
+
+		public override FilterDecision Decide(LoggingEvent loggingEvent)
+		{
+			var result = FilterDecision.Deny;
+			var logLevel = (LogLevel)Enum.Parse(typeof(LogLevel), loggingEvent.Level.Name);
+			var levelToLog = (LogLevel)Enum.Parse(typeof(LogLevel), Level.Name);
+
+			if ((int)logLevel >= (int)levelToLog)
+			{
+				result = FilterDecision.Accept;
+			}
+
+			return result;
+		}
+	}
+}
