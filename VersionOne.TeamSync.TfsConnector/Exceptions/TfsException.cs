2a60ed89972b73cf6868550a291c168b46927ad0
Valentin Plechuc <vplechuc@gmail.com>
2016-01-08 14:45:46 -0300

    S-53592: added TFS connector project

---
+++ VersionOne.TeamSync.TfsConnector/Exceptions/TfsException.cs
@@ -0,0 +1,20 @@
+﻿/*(c) Copyright 2012, VersionOne, Inc. All rights reserved. (c)*/
+
+using System;
+using System.Net;
+
+namespace VersionOne.TeamSync.TfsConnector.Exceptions
+{
+    public class TfsException : Exception
+    {
+        public HttpStatusCode StatusCode { get; private set; }
+
+        public TfsException(string message) : base(message) { }
+        public TfsException(string message, Exception innerException) : base(message, innerException) { }
+        public TfsException(HttpStatusCode statusCode, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
