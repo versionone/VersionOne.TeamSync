c6875dcf39d5acd9ff6fb246bc0a6ef7619d30c5
Josh Gough <jsgough@gmail.com>
2016-01-12 17:27:40 -0500

    S-53592 Introduced ITfsConnectorFactory to break hard dependencies on the Connector from the Worker

---
+++ VersionOne.TeamSync.TfsConnector/Connector/TfsConnectorFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VersionOne.TeamSync.Interfaces;
+using VersionOne.TeamSync.TfsConnector.Config;
+using VersionOne.TeamSync.TfsConnector.Interfaces;
+
+namespace VersionOne.TeamSync.TfsConnector.Connector
+{
+	public class TfsConnectorFactory : ITfsConnectorFactory
+	{
+		private IV1LogFactory _v1LogFactory;
+
+		[ImportingConstructor]
+		public TfsConnectorFactory([Import] IV1LogFactory v1LogFactory)
+		{
+			_v1LogFactory = v1LogFactory;
+		}
+
+		public ITfsConnector Create(TfsServer server)
+		{
+			return new TfsConnector(server, _v1LogFactory);
+		}
+	}
+}
